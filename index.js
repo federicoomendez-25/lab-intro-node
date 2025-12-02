7565796f7a7f8ad324a307fb8ff017e4d1d6d1ff
Federico <federicoo.mendezl@gmail.com>
2025-12-02 09:04:42 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  constructor() {
+  this.items = [];
+  this.length = 0;
 }
+
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+   if (pos < 0 || pos >= this.length) {
+     throw new Error('OutOfBounds');
+   }
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    } 
+    return this.items[this.length - 1];
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
+
+  sum() {
+    if (this.length === 0) return 0;
+    return this.items.reduce((acc, curr) => acc + curr, 0); 
+    
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+  }
+    return this.sum() / this.length;
+}
+
+}
+
 
 module.exports = SortedList;
